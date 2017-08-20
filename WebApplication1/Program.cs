ce763d4324f5b248049781e511f8078458782fa3
hhy <1300776935@qq.com>
2017-08-20 14:02:41 +0800

    9

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace WebApplication1
 {
-    public class Program
+    public class  Program
     {
         public static void Main(string[] args)
         {
